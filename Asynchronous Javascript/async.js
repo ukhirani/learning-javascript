3d453cdf9d9f382f7fe63e261efc5493521b0c2a
Umang Hirani <umanghirani.exe@gmail.com>
2025-10-26 00:03:11 +0530

    setting up linting and formattinf for js and ts in nvim

--- Asynchronous Javascript/async.js
+++ Asynchronous Javascript/async.js
@@ -1,10 +1,10 @@
-function complete(name){
+function complete(name) {
   // return function which prints string 'function complete' and name argument
-  return function (){console.log('function complete: ',name)};
+  return function() { console.log('function complete: ', name) };
 }
 // setTimeout for 5000ms = 5 seconds
-setTimeout(complete('setTimeout 5000'), 5000.0); 
+setTimeout(complete('setTimeout 5000'), 5000.0);
 // setTimeout for 2000ms = 2 seconds
-setTimeout(complete('setTimeout 2000'), 2000.0); 
+setTimeout(complete('setTimeout 2000'), 2000.0);
 // print 'End of program'
 console.log('End of program');
---
+++ Asynchronous Javascript/async.ts
@@ -0,0 +1,10 @@
+
+// setTimeout for 5000ms = 5 seconds
+// print 'End of program'
+console.log('End of program');
+
+function add(x: number, y: number): number {
+  return x + y;
+}
+
+console.log(add(5, 3)); // Output: 8
